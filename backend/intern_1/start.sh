8f3fc394ac8d146751b6818d345b613a360f8575
Eslam99Emam <eslam.emam.hussien@gmail.com>
2025-12-05 23:57:07 +0200

    updated deployment scripts

--- backend/intern_1/start.sh
+++ backend/intern_1/start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 pwd
-tree -L 3
-python --version
-which python
-which pip
 python manage.py makemigrations
 python manage.py migrate
-gunicorn backend.intern_1.wsgi:application --bind 0.0.0.0:$PORT
+python manage.py runserver
