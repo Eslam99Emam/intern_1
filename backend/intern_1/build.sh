b2a59e1c7796fcdda9314d6d0b06c60271bbcd09
Eslam99Emam <eslam.emam.hussien@gmail.com>
2025-12-05 23:41:02 +0200

    updated deployment scripts

--- backend/intern_1/build.sh
+++ backend/intern_1/build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-python --version
-pip install --upgrade pip
 pip install -r requirements.txt
--- backend/intern_1/start.sh
+++ backend/intern_1/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pwd
+source venv/Scripts/activate
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
