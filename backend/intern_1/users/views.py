34bda29c713b3509f3d692b99a0a75fe6983ff96
Eslam99Emam <eslam.emam.hussien@gmail.com>
2025-12-06 01:02:56 +0200

    updated deployment scripts

--- backend/intern_1/users/views.py
+++ backend/intern_1/users/views.py
@@ -5,7 +5,6 @@ import jwt
 from datetime import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import AccessToken
 from attempts.services import AttemptService
 from config import settings
 from users.serializers import UserSerializer
